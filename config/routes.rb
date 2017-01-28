bcb2844acb54e3b4334c1f26da944354842e40f7
Guilherme Sehn <ghsehn@gmail.com>
2017-01-28 16:28:16 -0200

    add projects scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :projects
   devise_for :users
+
   root to: 'home#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128181520) do
+ActiveRecord::Schema.define(version: 20170128182357) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
