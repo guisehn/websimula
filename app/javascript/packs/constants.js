9e220e7cce4e21e7362c72141d8221e79a8e0915
Guilherme Sehn <ghsehn@gmail.com>
2017-10-19 03:07:22 -0200

    use es6 'export default' on constants.js

--- app/javascript/packs/constants.js
+++ app/javascript/packs/constants.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   STAGE_SIZE: 25, // amount of positions for row/column
   AGENT_SIZE: 16, // square, in pixels
   DEFAULT_SPEED: 200 // initial simulation speed, in miliseconds
