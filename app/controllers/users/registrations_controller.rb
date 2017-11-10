ab7851e69b338f4b8bfe3009bccb3bed9bc3725a
Guilherme Sehn <ghsehn@gmail.com>
2017-11-10 16:30:33 -0200

    add name field to user settings page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -60,7 +60,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     # If you have extra params to permit, append them to the sanitizer.
     def configure_account_update_params
-      devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
 
     # The path used after sign up.
