3ab5ebf82ece4bbc741c05f1db797f442466d81c
Guilherme Sehn <ghsehn@gmail.com>
2017-11-14 13:51:10 -0200

    move 'simulaEditableOptions' function to webpacker folder, add comments to all imports on application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,35 +18,3 @@
 //= require spectrum
 //= require spectrum.pt-br
 //= require_tree .
-
-window.simulaEditableOptions = function () {
-  return {
-    url: function (params) {
-      var inner = {};
-      inner[$(this).data('property')] = params.value;
-
-      var data = {};
-      data[$(this).data('model')] = inner;
-
-      return $.ajax({
-        type: 'PUT',
-        url: $(this).data('model-url'),
-        dataType: 'json',
-        data: data
-      });
-    },
-
-    error: function(response, newValue) {
-      var message = '';
-      var prop = $(this).data('property');
-
-      if (response.responseJSON && response.responseJSON[prop]) {
-        message = response.responseJSON[prop][0];
-      } else {
-        message = 'Ocorreu um erro. Por favor, tente novamente.';
-      }
-
-      return message;
-    }
-  };
-};
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 /* eslint no-console:0 */
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -7,7 +8,19 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import './image-editor'
+// This module manages the project dashboard and the internal project pages, keeping track
+// of the real time updates made by other users
 import './project'
+
+// Image editor plugin (used to draw the agent images)
+import './image-editor'
+
+// Simulator code
 import './simulation/simulator'
+
+// Manages the load of vue.js components
 import './vue'
+
+// Defines some global helper functions that are used on pages that have some inline javascript
+// such as the project settings page, edit initial positions page, etc.
+import './global-helpers'
