7ebe21035dd9d15188da6d1ddd39daa059ac39c9
Guilherme Sehn <ghsehn@gmail.com>
2017-11-14 13:53:18 -0200

    update project name on emails

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -1,7 +1,7 @@
 class ProjectMailer < ApplicationMailer
   def registration_invite(project_invite)
     @project_invite = project_invite
-    @subject = "Simula - Convite para o projeto #{project_invite.project.name}"
+    @subject = "Web Simula - Convite para o projeto #{project_invite.project.name}"
     @url = new_user_registration_url(invite: @project_invite.code)
 
     mail(to: @project_invite.email, subject: @subject)
