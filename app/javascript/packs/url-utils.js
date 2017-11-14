a38f1d7a22481061c2e48a281599eac30e2c8be7
Guilherme Sehn <ghsehn@gmail.com>
2017-11-14 12:03:11 -0200

    first take using rails url helpers instead of hardcoded urls in javascript files

---
+++ app/javascript/packs/url-utils.js
@@ -0,0 +1,29 @@
+import _ from 'lodash'
+
+const PARAM_REGEX = /\:([^:&?\/]+)/g
+
+// Example:
+// match('/projects/5/agents/9', '/projects/:projectId/agents/:agentId')
+// returns { projectId: 5, agentId: 9 }
+function match(url, pattern) {
+  let paramNames = (pattern.match(PARAM_REGEX) || []).map(p => p.replace(':', ''))
+  let regex = _.escapeRegExp(pattern).replace(PARAM_REGEX, '([^&?\\/]+)')
+  let matches = url.match(regex)
+
+  return matches ? _.zipObject(paramNames, matches.slice(1)) : null
+}
+
+// Example:
+// generate('/projects/:projectId/agents/:agentId', { projectId: 5, agentId: 9 })
+// returns '/projects/5/agents/9'
+function generate(url, params) {
+  _.forEach(params, (value, key) => {
+    let escapedKey = _.escapeRegExp(key)
+    let regex = new RegExp(`:${escapedKey}`, 'g')
+    url = url.replace(regex, value)
+  })
+
+  return url
+}
+
+export default { match, generate }
